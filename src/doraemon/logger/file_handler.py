6dfdc7d9231083f2dc2c3dd7f55dabe3f15bcca1
Duan-JM <vincent.duan95@gmail.com>
2026-01-12 15:35:11 +0800

    config: change logger to time logger

--- src/doraemon/logger/file_handler.py
+++ src/doraemon/logger/file_handler.py
@@ -1,9 +1,21 @@
 import logging
-from logging.handlers import RotatingFileHandler
+from logging.handlers import TimedRotatingFileHandler
 
 
-def get_file_handler(log_level: int, file_name: str):
-    handler = RotatingFileHandler(file_name, maxBytes=10 * 1024 * 1024, backupCount=5)
+def get_file_handler(
+    log_level: int,
+    file_name: str,
+    rotation_when="midnight",
+    rotation_interval=1,
+    backup_count=30,
+):
+    handler = TimedRotatingFileHandler(
+        file_name,
+        when=rotation_when,
+        interval=rotation_interval,
+        backupCount=backup_count,
+        encoding="utf-8",
+    )
     handler.setLevel(log_level)
     handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
     return handler
